00bcaf0efc2337cfc5017bff48019205f866715f
John <32228851+sh98@users.noreply.github.com>
2017-11-03 19:03:06 +0000

    bot.js (not public)
    ask me for the extra code on discord

---
+++ bot-file.js
@@ -0,0 +1,9 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+Bot is not public
+
